6563e5e9ad1a745c1363e871071456a148a1bf4a
Wenxi Wang <esctrionsit@hotmail.com>
2026-04-15 16:50:52 +0800

    bugfix

--- src/09.BuildGeneticMap/03.merge.R
+++ src/09.BuildGeneticMap/03.merge.R
@@ -3,17 +3,21 @@ args <- commandArgs(trailingOnly = TRUE)
 chrcount <- as.numeric(args[1])
 
 library(qtl)
-for (chrid in 1:chrcount){
-	ssug <- read.cross("csv", "GM/", paste("GM.", as.character(chrid), ".csv", sep=""), genotypes=c("GG", "GT", "TT"), alleles=c("G", "T"))
+
+fullmap <- read.cross("csv", "GM/", "data.merged.csv", genotypes=c("GG", "GT", "TT"), alleles=c("G", "T"))
+chrs <- chrnames(fullmap)
+
+for (chrid in chrs){
+	ssug <- read.cross("csv", "GM/", paste("GM.", chrid, ".csv", sep=""), genotypes=c("GG", "GT", "TT"), alleles=c("G", "T"))
 	ssug2 <- subset(ssug, chr=chrid)
-	write.cross(ssug2, format="csv", filestem=paste("GM/GM.s.", as.character(chrid), sep=""))
+	write.cross(ssug2, format="csv", filestem=paste("GM/GM.s.", chrid, sep=""))
 }
 
 
-data <- read.csv("GM/GM.s.1.csv", header=F, stringsAsFactors = F)
-if (chrcount > 1){
-	for (chrid in 2:chrcount){
-		data2 <- read.csv(paste("GM/GM.s.", as.character(chrid), ".csv", sep=""),header=F, stringsAsFactors = F)
+data <- read.csv(paste("GM/GM.s.", chrs[1],".csv"), header=F, stringsAsFactors = F)
+if (length(chrs) > 1){
+	for (chridx in 2:length(chrs)){
+		data2 <- read.csv(paste("GM/GM.s.", chrs[chridx], ".csv", sep=""),header=F, stringsAsFactors = F)
 		data <- cbind(data, data2[-c(1)])
 	}
 }
